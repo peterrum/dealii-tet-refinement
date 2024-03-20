79c4f622a34029282e57df4e58c7d885aa08fbb2
Peter Munch <peterrmuench@gmail.com>
2024-03-20 15:20:41 +0100

    Add diferent refinement configurations

--- refinement.py
+++ refinement.py
@@ -66,23 +66,44 @@ def print_cell_face_vertices_tet(subcell_definititions_vertices):
     
 
 def main():
+    c = 2
+
     subline_definitions_vertics = [[0,4],[4,1],[1,5],[5,2],[0,6],[6,2],
                                    [0,7],[7,3],[1,8],[8,3],[2,9],[9,3],
                                    [4,5],[5,6],[6,4],
                                    [4,7],[7,8],[8,4],
                                    [6,9],[9,7],[7,6],
-                                   [5,8],[8,9],[9,5],
-                                   [6,8]] 
+                                   [5,8],[8,9],[9,5]]
+
+    if c == 0:
+      subline_definitions_vertics = subline_definitions_vertics + [[6,8]] 
+    if c == 1:
+      subline_definitions_vertics = subline_definitions_vertics + [[5,7]] 
+    if c == 2:
+      subline_definitions_vertics = subline_definitions_vertics + [[4,9]] 
     
-    subface_definitions_vertics = [[6,4,7],[4,5,8],[5,6,9],[7,8,9], 
-                                   [4,6,8],[6,5,8],[8,7,6],[9,6,8],
-                                   [0,4,6],[4,1,5],[5,2,6],[4,5,6],
+    subface_definitions_vertics = [[6,4,7],[4,5,8],[5,6,9],[7,8,9]]
+
+    if c == 0:
+      subface_definitions_vertics = subface_definitions_vertics + [[4,6,8],[6,5,8],[8,7,6],[9,6,8]]
+    if c == 1:
+      subface_definitions_vertics = subface_definitions_vertics + [[5,4,7],[6,5,7],[8,7,5],[7,9,5]]
+    if c == 2:
+      subface_definitions_vertics = subface_definitions_vertics + [[5,4,9],[4,6,9],[7,4,9],[4,8,9]]
+
+    subface_definitions_vertics = subface_definitions_vertics + [[0,4,6],[4,1,5],[5,2,6],[4,5,6],
                                    [1,4,8],[4,0,7],[8,7,3],[4,7,8],
                                    [0,6,7],[6,2,9],[7,9,3],[6,9,7],
                                    [2,5,9],[5,1,8],[9,8,3],[5,8,9]]
     
-    subcell_definititions_vertices = [[0,4,6,7], [4,1,5,8], [6,5,2,9], [7,8,9,3],
-                                      [4,5,6,8], [4,7,8,6], [6,9,7,8], [5,8,9,6]]
+    subcell_definititions_vertices = [[0,4,6,7], [4,1,5,8], [6,5,2,9], [7,8,9,3]]
+    
+    if c == 0:
+      subcell_definititions_vertices = subcell_definititions_vertices + [[4,5,6,8], [4,7,8,6], [6,9,7,8], [5,8,9,6]]
+    if c == 1:
+      subcell_definititions_vertices = subcell_definititions_vertices + [[4,5,6,7], [4,7,8,5], [6,9,7,5], [5,8,9,7]]
+    if c == 2:
+      subcell_definititions_vertices = subcell_definititions_vertices + [[4,5,6,9], [4,7,8,9], [6,9,7,4], [5,8,9,4]]
                                       
                                       
     print_face_lines_tet(subface_definitions_vertics[0:8],subline_definitions_vertics[12:])
